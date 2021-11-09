8bb40f7292e21dd5d45e87b0f1bb26568e1ac3da
alexdolg1967 <alexdolg1967@mail.ru>
2021-11-09 15:30:36 +0300

    workong header, home, lampoffer

--- src/js/main.js
+++ src/js/main.js
@@ -110,8 +110,7 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(`.home__data`);
-sr.reveal(`.home__img`, { delay: 500 });
-sr.reveal(`.home__social`, { delay: 600 });
+sr.reveal(`.lampoffer__container`, { delay: 300, origin: "left" });
 sr.reveal(`.about__img, .contact__box`, { origin: "left" });
 sr.reveal(`.about__data, .contact__form`, { origin: "right" });
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
